9e1e441ff61683cf0589322acd9aa8f9f1384ee2
yushuoqi <yushuoqi@kylinos.cn>
2022-05-19 15:42:04 +0800

    update clear.sh

--- clear.sh
+++ clear.sh
@@ -2,8 +2,14 @@
 
 make clean
 
-rm -f kylin-music.pro.user \
-    debian/files
-
-rm -rf kylin-music-plugins-simple/build/
-
+rm -rf kylin-music-plugins-simple/build/ \
+    kylin-music.pro.user \
+    debian/files \
+    .qmake.stash \
+    Makefile \
+    debian/.debhelper/ \
+    debian/debhelper-build-stamp \
+    debian/kylin-music.debhelper.log \
+    debian/kylin-music.substvars \
+    debian/kylin-music/ \
+    kylin-music
